9039dc19912605e0198ba138f85d2a092de795d4
Toyash <toyashpatil17@gmail.com>
2024-01-17 18:08:28 +0530

    init: folder structure

--- rubix24/src/App.jsx
+++ rubix24/src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import "./App.css";
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div className="bg-red-500 max-h-screen">Vite + React</div>
-    </>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- rubix24/src/main.jsx
+++ rubix24/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+import "./global.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
