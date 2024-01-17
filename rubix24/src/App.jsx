2194a2a53d8f21e8ace71830c58906aaafd40961
Toyash <toyashpatil17@gmail.com>
2024-01-17 19:42:21 +0530

    init: firebase

--- rubix24/src/App.jsx
+++ rubix24/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { collection, addDoc } from "firebase/firestore";
+import { db } from '../firebase';
+
+
+
 
 const App = () => {
+
+
+
+  const checkFire = async () => {
+    // Add a new document with a generated id.
+    const docRef = await addDoc(collection(db, "cities"), {
+      name: "Tokyo",
+      country: "Japan"
+    });
+    console.log("Documnet added")
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <button onClick={checkFire}>Press me</button>
+    </div>
   )
 }
 
