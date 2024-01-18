48e69c169e73dc00f774d1aee2ba0761019e798b
Jiya Trivedi <jiyatrivedi204@gmail.com>
2024-01-18 20:06:55 +0530

    feat:GoogleSignIn

--- rubix24/src/screens/Login.jsx
+++ rubix24/src/screens/Login.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
 import { Button } from '../components/Button';
 import GoogleButton from 'react-google-button';
+import authContext from '../context/authContext';
 const Login = () => {
   
+  const { GoogleSignIn } = useContext(authContext);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const handleGoogleSignIn = async () => {
+    try {
+      setError('');
+      setLoading(true);
+      await GoogleSignIn();
+      console.log('hii'); 
+    } catch (error) {
+      console.error('Google Sign-In Failed', error);
+    }
+    setLoading(false);
+  };
   return (
     <div className='login flex flex-shrink-0 flex-col-reverse '>
       <div className='form h-auto w-full bg-[#141414] flex-col   justify-center items-center @media(max-width: 768px){ py-[5%] px-[4%] }'>
@@ -30,7 +45,7 @@ const Login = () => {
           <Button onClick={console.log("hello")} className=" button ">Login</Button>
           </form>
           
-          <div className='G flex w-full justify-center mt-5 ' ><GoogleButton type='light' /></div>
+          <div className='G flex w-full justify-center mt-5 '  onClick={handleGoogleSignIn} ><GoogleButton type='light' /></div>
         
         
       </div>
