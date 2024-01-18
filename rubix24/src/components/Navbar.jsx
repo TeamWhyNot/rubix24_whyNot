e34287e8ac072727ade1e5875e453826b5380188
Toyash <toyashpatil17@gmail.com>
2024-01-18 19:44:27 +0530

    refactor:Navbar

--- rubix24/src/components/Navbar.jsx
+++ rubix24/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className=' border-solid border-2 p-2 rounded-lg border-[#F8D600] w-[30%] gap-10 flex items-center justify-center '>
+    <div className=' border-solid border-2 p-2 flex-wrap rounded-lg border-[#F8D600] w-[30%] gap-10 flex items-center justify-center '>
         <div className=' bg-[#1A1A1A] text-[14px] p-2 text-white hover:shadow-2xl rounded-md '>
             <Link to="/"><h1>Home</h1></Link>
         </div>
