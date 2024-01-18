2e9846e145bed309d107a83b977022e650acb0c0
Jiya Trivedi <jiyatrivedi204@gmail.com>
2024-01-18 19:33:56 +0530

    feat:loginPage

---
+++ rubix24/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Button = ({className,onClick, children}) => {
+  return (
+    // <button className='button color-black flex justify-center items-center w-[70%] h-[4rem] bg-gold rounded-[0.7rem] '>Login</button>
+    <>
+    <button onClick={onClick} className={`color-black flex justify-center items-center w-[100%] h-[4rem] bg-gold rounded-[0.7rem] ${className}`}>
+      {children}
+    </button>
+   </>
+    )
+}
--- rubix24/src/screens/Login.jsx
+++ rubix24/src/screens/Login.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
-
+import { Button } from '../components/Button';
+import GoogleButton from 'react-google-button';
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login flex flex-shrink-0 flex-col-reverse '>
+      <div className='form h-auto w-full bg-[#141414] flex-col   justify-center items-center @media(max-width: 768px){ py-[5%] px-[4%] }'>
+        <div className="header text-white">
+          <h1 className=' text-2xl font-bold mb-[1.5rem] @media (min-width: 768px) { text-xl } '>Login</h1>
+        </div>
+        <form className="login-form flex flex-col gap-8 mb-" >
+        <div className="mail flex flex-col gap-2">
+          <div className="heading text-xl text-white @media (max-width: 768px){ text-lg } ">Email</div>
+          <input 
+            type="email"
+            placeholder="Enter your email address"
+            
+            required
+          />
+          </div>
+          <div className="pass flex flex-col gap-2">
+          <div className="heading text-xl text-white @media(max-width: 768px){ text-lg} ">Password</div>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            required
+          />
+          </div>
+          <Button onClick={console.log("hello")} className=" button ">Login</Button>
+          </form>
+          
+          <div className='G flex w-full justify-center mt-5 ' ><GoogleButton type='light' /></div>
+        
+        
+      </div>
+    </div>
   )
 }
 
